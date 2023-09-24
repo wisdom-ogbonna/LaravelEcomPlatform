518eee21569d96fb2d3259a8e3ef95d8a668f635
wisdom ogbonna <doph574@gmail.com>
2023-09-24 07:44:40 +0100

    Guest Layout + Login, Signup

--- laravelEcom/frontend/src/components/GuestLayout.jsx
+++ laravelEcom/frontend/src/components/GuestLayout.jsx
@@ -11,7 +11,6 @@ function GuestLayout() {
 
   return (
     <div>
-        <h1>Hello</h1>
         <Outlet />
     </div>
   )
--- laravelEcom/frontend/src/views/Signup.jsx
+++ laravelEcom/frontend/src/views/Signup.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {Link} from "react-router-dom";
 
-function Signup() {
+export default function Signup() {
+
+
+  const onSubmit = ev => {
+    ev.preventDefault()
+  }
+
   return (
-    <div>Signup</div>
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Signup for Free</h1>
+          <input  type="text" placeholder="Full Name"/>
+          <input  type="email" placeholder="Email Address"/>
+          <input  type="password" placeholder="Password"/>
+          <input  type="password" placeholder="Repeat Password"/>
+          <button className="btn btn-block">Signup</button>
+          <p className="message">Already registered? <Link to="/login">Sign In</Link></p>
+        </form>
+      </div>
+    </div>
   )
 }
-
-export default Signup
