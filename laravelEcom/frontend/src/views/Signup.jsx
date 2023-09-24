384943f3ae68bf56b5d1d16b04178c061da133db
wisdom ogbonna <doph574@gmail.com>
2023-09-24 10:38:43 +0100

    Implement Signup in React

--- laravelEcom/frontend/src/views/Signup.jsx
+++ laravelEcom/frontend/src/views/Signup.jsx
@@ -1,10 +1,40 @@
+import { useRef } from "react";
 import {Link} from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../context/ContextProvider";
 
 export default function Signup() {
 
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+  
+  const {setUser, setToken} = useStateContext()
 
   const onSubmit = ev => {
     ev.preventDefault()
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    axiosClient.post('/signup', payload)
+    .then(({ data }) => {
+      setUser(data.user);  // Update user data
+      setToken(data.token);  // Update token data
+    })
+      .catch(err => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          setErrors(response.data.errors)
+        }
+      })
+  
+    
+    
   }
 
   return (
@@ -12,10 +42,10 @@ export default function Signup() {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup for Free</h1>
-          <input  type="text" placeholder="Full Name"/>
-          <input  type="email" placeholder="Email Address"/>
-          <input  type="password" placeholder="Password"/>
-          <input  type="password" placeholder="Repeat Password"/>
+          <input ref={nameRef} type="text" placeholder="Full Name"/>
+          <input ref={emailRef} type="email" placeholder="Email Address"/>
+          <input ref={passwordRef} type="password" placeholder="Password"/>
+          <input ref={passwordConfirmationRef} type="password" placeholder="Repeat Password"/>
           <button className="btn btn-block">Signup</button>
           <p className="message">Already registered? <Link to="/login">Sign In</Link></p>
         </form>
