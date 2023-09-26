c853197fcc93fb7f757c1fcb5fa6edded750f8f4
wisdom ogbonna <doph574@gmail.com>
2023-09-26 10:59:04 +0100

    Implement Login

--- laravelEcom/frontend/src/views/Login.jsx
+++ laravelEcom/frontend/src/views/Login.jsx
@@ -1,9 +1,45 @@
+import { createRef, useState } from "react";
 import {Link} from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../context/ContextProvider";
 
 export default function Login() {
+  const {setUser, setToken} = useStateContext()
+  const [errors, setErrors] = useState(null)
+
+  const emailRef = createRef()
+  const passwordRef = createRef()
 
   const onSubmit = ev => {
     ev.preventDefault()
+
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+
+    }
+
+    setErrors(null)
+
+    axiosClient.post('/login', payload)
+    .then(({data}) => {
+      setUser(data.user)
+      setToken(data.token);
+    })
+    .catch(err => {
+      const response = err.response;
+      if (response && response.status === 422) {
+
+        if(response.data.error){
+          setErrors(response.data.errors)
+        }else{
+          setErrors({
+            email: [response.data.message]
+          })
+        }
+        
+      }
+    })
 
 
   }
@@ -13,8 +49,15 @@ export default function Login() {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Login into your account</h1>
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
+          {errors &&
+            <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          }
+          <input ref={emailRef} type="email" placeholder="Email"/>
+          <input ref={passwordRef} type="password" placeholder="Password"/>
           <button className="btn btn-block">Login</button>
           <p className="message">Not registered? <Link to="/signup">Create an account</Link></p>
         </form>
