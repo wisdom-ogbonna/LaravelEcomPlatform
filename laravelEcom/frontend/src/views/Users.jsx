7d34e85abcc4fa282702c2d7152084eba3b88912
wisdom ogbonna <doph574@gmail.com>
2023-09-28 16:58:06 +0100

    Users CRUD in API Backend

--- laravelEcom/frontend/src/views/Users.jsx
+++ laravelEcom/frontend/src/views/Users.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../axios-client';
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const getUser = () => {
+    setLoading(true)
+    axiosClient.get('/users')
+      .then(([data]) => {
+        setLoading(false)
+        console.log(data)
+      })
+      .catch(()=>{
+        setLoading(false)
+      })
+  }
+
   return (
     <div>Users Dashboard</div>
   )
--- laravelEcom/routes/api.php
+++ laravelEcom/routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/users', UserController::class);
  
 });
 Route::post('/signup', [AuthController::class, 'signup']);
